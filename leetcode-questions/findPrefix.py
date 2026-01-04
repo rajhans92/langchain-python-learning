d086b3534924b539d22f1b6ff60c5359f04192ae
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-01-04 18:59:05 +0530

    find long prefix with best solution

--- leetcode-questions/findPrefix.py
+++ leetcode-questions/findPrefix.py
@@ -15,3 +15,25 @@ class Solution:
             preFix = preFix+""+ strs[j][i]
 
         return preFix
+    
+#=========================================== best solution ==============================
+"""
+class Solution:
+def longestCommonPrefix(self, strs: List[str]) -> str:
+
+    strs.sort()
+    prefix= ""
+    firstWord = strs[0]
+    lastWord = strs[len(strs)-1]
+
+    for i in range(len(firstWord)):
+        if firstWord[i] != lastWord[i]:
+            return prefix
+        prefix = prefix+""+firstWord[i]
+
+    return prefix
+                    
+
+"""
+
+        
